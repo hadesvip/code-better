8876bc02d744c2b0bb6ddf8b3775e2ec0e4b3252
wangyong <yundan_wy@126.com>
2021-06-26 21:34:51 +0800

    添加测试类

--- dev-common/src/test/java/com/kevin/dev/common/utils/OkHttpUtilsTests.java
+++ dev-common/src/test/java/com/kevin/dev/common/utils/OkHttpUtilsTests.java
@@ -1,5 +1,6 @@
 package com.kevin.dev.common.utils;
 
+import okhttp3.Call;
 import org.junit.Test;
 
 import java.util.UUID;
@@ -19,6 +20,38 @@ public class OkHttpUtilsTests {
                 .get()
                 .sync();
         System.out.println(resJson);
+    }
+
+
+    @Test
+    public void postTest(){
+        OkHttpUtils.builder().url("请求地址，http/https都可以")
+                // 有参数的话添加参数，可多个
+                .addParam("参数名", "参数值")
+                .addParam("参数名", "参数值")
+                // 也可以添加多个
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                // 如果是true的话，会类似于postman中post提交方式的raw，用json的方式提交，不是表单
+                // 如果是false的话传统的表单提交
+                .post(true)
+                .sync();
+
+        // 选择异步有两个方法，一个是带回调接口，一个是直接返回结果
+        OkHttpUtils.builder().url("")
+                .post(false)
+                .async();
+
+        OkHttpUtils.builder().url("").post(false).async(new OkHttpUtils.CallBack() {
+            @Override
+            public void onSuccessful(Call call, String data) {
+
+            }
+
+            @Override
+            public void onFailure(Call call, String errorMsg) {
+                // 请求失败后的处理
+            }
+        });
     }
 
 
