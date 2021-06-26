f24f5c7192eb4c28a7b96e1d076f60b2a0ef92b7
wangyong <yundan_wy@126.com>
2021-06-26 21:36:13 +0800

    添加测试类

---
+++ dev-web/src/main/java/com/kevin/dev/web/dev/web/DevWebApplication.java
@@ -0,0 +1,17 @@
+package com.kevin.dev.web.dev.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author wangyong
+ */
+@SpringBootApplication
+public class DevWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevWebApplication.class, args);
+    }
+
+}
---
+++ dev-web/src/test/java/com/kevin/dev/web/dev/web/DevWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.dev.web.dev.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
