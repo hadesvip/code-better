663fde7f2082075aca92a0f8502f734071793e4d
wangyong <yundan_wy@126.com>
2021-06-26 19:32:52 +0800

    修改目录

--- dev-common/src/test/java/com/kevin/dev/common/utils/OkHttpUtilsTests.java
+++ dev-common/src/test/java/com/kevin/dev/common/utils/OkHttpUtilsTests.java
@@ -1,12 +1,18 @@
 package com.kevin.dev.common.utils;
 
+import org.junit.Test;
+
 /**
  * okHttp工具类测试
  * @author wangyong
  */
 public class OkHttpUtilsTests {
 
+    @Test
+    public void getTest(){
 
-    
+
+    }
+
 
 }
