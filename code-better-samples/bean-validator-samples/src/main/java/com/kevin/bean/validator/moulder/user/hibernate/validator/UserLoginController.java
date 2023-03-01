dbc2cb062847732fd702ce1245181e71da607d06
wangyong <wb_wangyong02@meituan.com>
2023-03-01 19:51:21 +0800

    添加示例代码

---
+++ code-better-samples/bean-validator-samples/src/main/java/com/kevin/bean/validator/moulder/user/hibernate/validator/UserLoginController.java
@@ -0,0 +1,16 @@
+package com.kevin.bean.validator.moulder.user.hibernate.validator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * TODO 用一句话描述这个类的作用
+ *
+ * @author kevin
+ */
+@RequestMapping("/api/user")
+public class UserLoginController {
+
+
+
+
+}
---
+++ code-better-samples/bean-validator-samples/src/main/java/com/kevin/bean/validator/moulder/user/hibernate/validator/dto/UserLoginDto.java
@@ -0,0 +1,9 @@
+package com.kevin.bean.validator.moulder.user.hibernate.validator.dto;
+
+/**
+ * TODO 用一句话描述这个类的作用
+ *
+ * @author wb_wangyong02
+ */
+public class UserLoginDto {
+}
